b76fb162ec3502c6c9613f0b039e6018e5e21c28
Travis Hession <travis@orbitinghq.net>
2018-06-09 18:00:32 -0700

    fixed write to file line

--- yamljson.py
+++ yamljson.py
@@ -8,8 +8,8 @@ list.append('route')
 list.append('switch')
 list.append({'vendor': 'cisco', 'platform': 'NX-OS'})
 
-with open('yaml.yml', 'w') as yaml:
-    yaml.dump(list, default_flow_style = False)
+with open('yaml.yml', 'w') as yamlfile:
+    yamlfile.write(yaml.dump(list, default_flow_style = False))
 
-with open('json.js' 'w') as json:
-    json.dump(list)
+with open('json.js' 'w') as jsonfile:
+    jsonfile.write(json.dump(list))
