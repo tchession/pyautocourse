7d5e871bf1695cfad7dfce4bf8099ee6e587befa
Travis Hession <travis@orbitinghq.net>
2018-06-09 17:57:40 -0700

    first attempt exercise 6

---
+++ yamljson.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import yaml
+import json
+
+list = range(9)
+list.append('route')
+list.append('switch')
+list.append({'vendor': 'cisco', 'platform': 'NX-OS'})
+
+with open('yaml.yml', '+w') as yaml:
+    yaml.dump(list, default_flow_style = False)
+
+with open('json.js' '+w') as json:
+    json.dump(list)
